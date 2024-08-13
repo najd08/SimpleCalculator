7873acdc77efc7a3a71dd3514d120dbaaa3bbad0
najd08 <120324320+najd08@users.noreply.github.com>
2024-08-13 20:47:26 +0300

    Add Xcode Playground file

---
+++ Playground5.playground/Contents.swift
@@ -0,0 +1,37 @@
+
+// + operation
+func plus(firstNum: Int, secondNum: Int) -> Int {
+    return firstNum + secondNum
+}
+
+var added = plus(firstNum: 50, secondNum: 10)
+print("The two numbers added = \(added)")
+
+
+// - operation
+func minus(firstNum: Int, secondNum: Int) -> Int {
+    return firstNum - secondNum
+}
+
+var subtracted = minus(firstNum: 50, secondNum: 10)
+print("The two numbers subtracted = \(subtracted)")
+
+
+// / operation
+func division(firstNum: Int, secondNum: Int) -> Int {
+    return firstNum / secondNum
+}
+
+
+var divided = division(firstNum: 50, secondNum: 10)
+print("The two numbers divided = \(divided)")
+
+
+// * operation
+func multiplication(firstNum: Int, secondNum: Int) -> Int {
+    return firstNum * secondNum
+}
+
+var multiplied = multiplication(firstNum: 50, secondNum: 10)
+print("The two numbers multiplied = \(multiplied)")
+
